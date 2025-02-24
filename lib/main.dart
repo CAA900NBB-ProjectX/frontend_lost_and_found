be9345764020d797f7eae27768ecdfb49398d64e
vishnurnath10 <vishnurnath367@gmail.com>
2025-02-24 15:10:11 -0500

    preparing code before merging

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'auth/screens/signup_page.dart';
-import 'screens/home/home_screen.dart';
-import 'screens/lost_item_screen.dart';
-import 'screens/found_item_screen.dart';
-import 'screens/profile_screen.dart';
 import 'auth/screens/login_page.dart';
 import 'auth/services/auth_service.dart';
 import 'auth/screens/verification_page.dart';
@@ -24,13 +20,11 @@ class MyApp extends StatelessWidget {
       ),
       home: const AuthenticationWrapper(),
       routes: {
-        '/home': (context) => const MainNavigator(),
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignupPage(),
         '/verification': (context) => VerificationPage(
             email: ModalRoute.of(context)!.settings.arguments as String
         ),
-        '/test': (context) => const HomeScreen(),
       },
     );
   }
@@ -72,60 +66,6 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
       );
     }
 
-    return _isAuthenticated ? const MainNavigator() : const LoginPage();
-  }
-}
-
-class MainNavigator extends StatefulWidget {
-  const MainNavigator({super.key});
-
-  @override
-  State<MainNavigator> createState() => _MainNavigatorState();
-}
-
-class _MainNavigatorState extends State<MainNavigator> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _screens = [
-    const HomeScreen(),
-    const LostItemScreen(),
-    const FoundItemScreen(),
-    const ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Lost',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: 'Found',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
-    );
+    return _isAuthenticated ? const Scaffold() : const LoginPage(); // Anas this is vishnu from here it should navigate to homescreen(mean item listing page that you have)
   }
 }
