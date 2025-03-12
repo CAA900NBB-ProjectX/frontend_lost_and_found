cee37993d2daee5ee4a05f23bf9a9164f0f4acbd
vishnurnath10 <vishnurnath367@gmail.com>
2025-03-12 18:35:01 -0400

    config file

---
+++ lib/config/api_config.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class ApiConfig {
+  static String get baseUrl {
+    return 'http://172.172.229.186:8085';
+  }
+  static String get authBaseUrl => '$baseUrl/auth';
+  static String get loginUrl => '$authBaseUrl/login';
+  static String get signupUrl => '$authBaseUrl/signup';
+  static String get verifyUrl => '$authBaseUrl/verify';
+  static String get resendCodeUrl => '$authBaseUrl/resend';
+
+  static String get userBaseUrl => '$baseUrl/user';
+  static String get userMeUrl => '$userBaseUrl/me';
+
+
+  static Map<String, String> get headers => {
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+  };
+}
