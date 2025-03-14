d5ab507b647b3cef35e130dab483216531c1ebb2
vishnurnath10 <vishnurnath367@gmail.com>
2025-03-14 17:14:48 -0400

    added endpoint

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,7 +2,8 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConfig {
   static String get baseUrl {
-    return 'http://172.206.240.144:8085';
+    // return 'http://172.206.240.144:8085';
+    return 'http://foundit.eastus.cloudapp.azure.com:8085';
   }
   static String get authBaseUrl => '$baseUrl/auth';
   static String get loginUrl => '$authBaseUrl/login';
@@ -13,6 +14,15 @@ class ApiConfig {
   static String get userBaseUrl => '$baseUrl/user';
   static String get userMeUrl => '$userBaseUrl/me';
 
+  // things to remove
+
+  // // Chat endpoints
+  // static String get chatBaseUrl => '$baseUrl/api/chats';
+  // static String get messageBaseUrl => '$baseUrl/api/messages';
+  //
+  // // WebSocket endpoint
+  // static String get wsUrl => 'wss://${baseUrl.replaceAll('https://', '')}/ws';
+  // //upto this
 
   static Map<String, String> get headers => {
     'Content-Type': 'application/json',
