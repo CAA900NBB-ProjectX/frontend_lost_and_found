b7a8ecc3db29e62c535ee1a3f0e7353973fecb09
Anas <anasahmed@live.ca>
2025-03-14 07:15:20 -0400

    Update API configuration with item service endpoints

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConfig {
   static String get baseUrl {
-    return 'http://172.206.240.144:8085';
+    return 'https://fb49-74-12-27-5.ngrok-free.app';
   }
   static String get authBaseUrl => '$baseUrl/auth';
   static String get loginUrl => '$authBaseUrl/login';
@@ -12,6 +12,18 @@ class ApiConfig {
 
   static String get userBaseUrl => '$baseUrl/user';
   static String get userMeUrl => '$userBaseUrl/me';
+
+  // Add Item API endpoints
+  static String get itemBaseUrl => '$baseUrl/item';
+  static String get getAllItemsUrl => '$itemBaseUrl/getallitems';
+  static String get insertItemUrl => '$itemBaseUrl/insertitems';
+  static String get getItemByIdUrl => '$itemBaseUrl/getitems'; // Will append /{id}
+  static String get uploadImageUrl => '$itemBaseUrl/uploadimage'; // Will append /{itemId}
+  static String get getImageUrl => '$itemBaseUrl/getimage'; // Will append /{imageId}
+  static String get updateItemUrl => '$itemBaseUrl/updateitem'; // Will append /{id}
+  static String get deleteItemUrl => '$itemBaseUrl/deleteitem'; // Will append /{id}
+  static String get getItemsByUserUrl => '$itemBaseUrl/getitemsbyuser'; // Will append /{userId}
+  static String get searchItemsUrl => '$itemBaseUrl/search'; // Will use query parameters
 
 
   static Map<String, String> get headers => {
